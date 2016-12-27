ba56b6ef3d231061b31100a18722e5ebfe74c9e0
Tien Nguyen <tiennt4@rever.vn>
2016-12-27 11:04:44 +0700

    Support start master and slave separated

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,21 +14,48 @@ sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > 
 
 # altering TMP_DIR
 TMP_DIR=${TMP_DIR:-/opt/hadoop}
-if [[ ! -d $TMP_DIR ]]; then
-	mkdir -p $TMP_DIR
+if [[ ! -d $TMP_DIR/data ]]; then
+	mkdir -p $TMP_DIR/data
 fi
-sed -i 's/TMP_DIR/$TMP_DIR/g' /usr/local/hadoop/etc/hadoop/core-site.xml
-
-# Formating name node if data dir is fresh
-if [[ ! -f $TMP_DIR/formated ]]; then
-	$HADOOP_PREFIX/bin/hdfs namenode -format
-	touch $TMP_DIR/formated
-fi
+sed -i "s;TMP_DIR;$TMP_DIR;g" /usr/local/hadoop/etc/hadoop/core-site.xml
 
 service sshd start
-$HADOOP_PREFIX/sbin/start-dfs.sh
-$HADOOP_PREFIX/sbin/start-yarn.sh
-$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
+
+case $1 in
+	all)
+	# Formating name node if data dir is fresh
+	if [[ ! -f $TMP_DIR/formated ]]; then
+		$HADOOP_PREFIX/bin/hdfs namenode -format
+		touch $TMP_DIR/formated
+	fi
+	$HADOOP_PREFIX/sbin/start-dfs.sh
+	$HADOOP_PREFIX/sbin/start-yarn.sh
+	$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
+	shift
+	;;
+	master)
+	# Formating name node if data dir is fresh
+	if [[ ! -f $TMP_DIR/formated ]]; then
+		$HADOOP_PREFIX/bin/hdfs namenode -format
+		touch $TMP_DIR/formated
+	fi
+	$HADOOP_PREFIX/sbin/hadoop-daemon.sh --script hdfs start namenode
+	$HADOOP_YARN_HOME/sbin/yarn-daemon.sh start resourcemanager
+	$HADOOP_YARN_HOME/sbin/yarn-daemon.sh start proxyserver
+	$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
+	shift
+	;;
+	slave)
+	$HADOOP_PREFIX/sbin/hadoop-daemons.sh --script hdfs start datanode
+	$HADOOP_YARN_HOME/sbin/yarn-daemons.sh start nodemanager
+	shift
+	;;
+	
+	*)
+	# Unknown option
+	exec $@
+	;;
+esac
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
